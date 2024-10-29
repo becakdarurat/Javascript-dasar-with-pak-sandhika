828a80b2a92af52d61d0e5b6328d2e77b7b936c7
becakdarurat <becakdarurat>
2024-10-29 07:33:24 +0700

    "belajar Function, declaration dan expression"

---
+++ suitjawa/Array.js
@@ -0,0 +1,8 @@
+// Array di Javascript
+let hari = [`Senin`, `Selasa`, `Rabu`];
+for (const day of hari) {
+    console.log(`${day}`);
+}
+
+console.log(typeof(hari));
+
---
+++ suitjawa/FunctionDeclaration.js
@@ -0,0 +1,8 @@
+// Function Declaration
+function tampilNama(name) {
+    console.log(`Hallo ${name}`);
+}
+tampilNama(`Sofyan`);
+
+const angkaGanjil = value => value % 2 == 0;
+console.log(angkaGanjil(100));
